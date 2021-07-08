79175592ee88f9b0cdeef24d8c1c24efd1c94cf6
unzh-boop <uneezehhabib@gmail.com>
2021-07-09 00:14:01 +0500

    commit test

--- fyp/project/login/urls.py
+++ fyp/project/login/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from login import views
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path("", views.index, name='index'),
@@ -10,4 +13,6 @@ urlpatterns = [
     path("upload", views.upload, name='upload'),
     path("gallery", views.gallery, name='gallery'),
     path("contact", views.contact, name='contact'),
+     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
--- fyp/project/project/settings.py
+++ fyp/project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h3(0#ce(o1#^o4av1qeh4uglvcke7j9riqcngcec*ue$w1bd3i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
